bb75565612f937aa56efa666622201f3033cf089
aarora <aarora@birg.unbc.ca>
2023-11-25 20:16:22 -0800

    Css_animation

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import Header from './components/Header'
 import Footer from './components/Footer'
 import BodySection from './components/BodySection'
+import Background from './Background';
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <BodySection></BodySection>
-      <Footer></Footer>
+      <div className='wrapper'>
+       <Background></Background>
+      </div>
+        <Header></Header>
+        <BodySection></BodySection>
+        <Footer></Footer>
     </div>
   );
 }
---
+++ src/Background.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Background() {
+    return <div >
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+}
+
+export default Background;
