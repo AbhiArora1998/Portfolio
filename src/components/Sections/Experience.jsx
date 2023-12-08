90b94f1a1cef76bb2042132c24f63bdf420754cb
aarora <aarora@birg.unbc.ca>
2023-12-07 19:25:29 -0800

    new_experience

--- src/components/Sections/Experience.jsx
+++ src/components/Sections/Experience.jsx
@@ -7,7 +7,7 @@ function Experience() {
         <div className="title">
             <h1 className="scrollAnimate">Experience</h1>
         </div>
-        <div className="ExperinceCardsFlexBox">
+        {/* <div className="ExperinceCardsFlexBox">
             <div className="ExperienceCard card1">
                 <div className="linearGradient">
                     <CardDetails data={experience[0]}></CardDetails>
@@ -17,6 +17,74 @@ function Experience() {
                 <div className="linearGradient">
                     <CardDetails data={experience[1]}></CardDetails>
                 </div>
+            </div>
+        </div> */}
+
+        <div className="experienceContainer">
+            <div className="experienceContiner1">
+                <div className="cardContainerExperience">
+                    <div className="cardExperience">
+                        <h3 className="experienceInfoTitle">
+                            Research Skilled Development Trainee
+                        </h3>
+                        <h4 className="experienceLocation">
+                            Bussiness Intelligence Research Group, UNBC
+                        </h4>
+                        <h4 style={{ textAlign: "center" }}>
+                            Tools
+                        </h4>
+                        <div className="exprienceToolsContainer">
+                            {experience[0].skills.map((value, index) => {
+                                return <div className="flexStyleforTools">
+                                    <h3 className="experienceTools">{value}</h3>
+                                </div>
+                            })}
+                        </div>
+                    </div>
+                </div>
+                <div className="experienceInfoContainer">
+                    <div className="experienceAboutInfoPadding">
+                        <h3 className="aboutStartupInfo " >
+                            April 2022 - Present
+                        </h3>
+                        <h3 className="aboutStartupInfo" style={{ margin: "5%" }}>
+                            {experience[0].content}
+                        </h3>
+                    </div>
+                </div>
+            </div>
+            <div className="experienceContiner2">
+                <div className="cardContainerExperience">
+                    <div className="cardExperience">
+                        <h3 className="experienceInfoTitle">
+                            Database Administrator Trainee
+                        </h3>
+                        <h4 className="experienceLocation">
+                            Northern HydroMetrological Group,UNBC
+                        </h4>
+                        <h4 style={{ textAlign: "center" }}>
+                            Tools
+                        </h4>
+                        <div className="exprienceToolsContainer">
+                            {experience[1].skills.map((value, index) => {
+                                return <div className="flexStyleforTools">
+                                    <h3 className="experienceTools">{value}</h3>
+                                </div>
+                            })}
+                        </div>
+                    </div>
+                </div>
+                <div className="experienceInfoContainer ">
+                    <div className="experienceAboutInfoPadding">
+                        <h3 className="aboutStartupInfo" style={{ textAlign: "end", padding: "1% 10% 2% 0%" }} >
+                            April 2022 - Present
+                        </h3>
+                        <h3 className="aboutStartupInfo" style={{ margin: "5%" }}>
+                            {experience[0].content}
+                        </h3>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
--- src/components/Sections/experienceObject.jsx
+++ src/components/Sections/experienceObject.jsx
@@ -19,12 +19,7 @@ const experience = [
         title: "Research Skilled Development Trainee",
         location: "Bussiness Intelgence Research Group, UNBC",
         content: [
-            "Built Single Page Appcations for cents using Angular and primeNg package, utizing expertise in MVC architecture, data binding, and UI component integration",
-            "Crafted and executed a streamned MS SQL database solution, resulting in a 30% reduction in data redundancy and a noticeable improvement in data retrieval efficiency.",
-            "Developed and maintained a new RESTful API for integrating the cent's website using .Net 6, improving data transfer and scalabity.",
-            "Strengthened API security by implementing Amazon Web Services (AWS) IAM for access control and AWS Lambda for real-time threat monitoring and response, resulting in a significant improvement in security risks and unauthorized access.",
-            "Utized AWS S3 integration within .NET 6 framework to optimize image storage and retrieval. Leveraged cloud- based infrastructure to enhance appcation performance by efficiently managing image assets.",
-            "Collaborated closely with cross-functional team members on GitLab to streamne upcoming changes, successfully devering new features and resolving bugs. Contributed to a 20% reduction in bug-related customer support inquiries by implementing targeted fixes and enhancements."
+       "I led the development of high-performance SPAs for cents using Angular and primeNg, optimized an MS SQL database by 30%, and created a scalable RESTful API with .Net 6. I enhanced security with AWS IAM and Lambda, optimized image storage via AWS S3 integration, and collaborated on GitLab for a 20% reduction in bug-related inquiries."
         ],
         skills: ["Angular", "Flutter", ".Net6", "SQL", "AWS", "Javacript", "HTML", "CSS", "Dart", "Git"],
         icons: [angular,flutter,net,sql,aws,js,html,css,dart,git]
