9efda011fa8f5d5b2f3c3df3752e14fe570639dc
aarora <aarora@birg.unbc.ca>
2023-12-10 14:26:36 -0800

    backgroujnd

--- src/Background.jsx
+++ src/Background.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import back from "./assets/images/herobg.png"
 function Background() {
     return <div className="grandientWrapper">
         {/* <span></span>
@@ -15,7 +15,7 @@ function Background() {
          */}
 
          <div className="randomGradient1">
-           
+           <img className="backGroundIMg" src={back} alt="" />
          </div>
     </div>
 }
