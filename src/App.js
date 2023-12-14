15e5c0a3fcc0a018c71b2103acb2786a1e67a3e6
aarora <aarora@birg.unbc.ca>
2023-12-14 15:39:46 -0800

    testkai

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
 
   return (
     <div className="App">
-      hello
+      hello kaiS
       {isLoading && <div ref={loader5Ref} id="loader" className="navigationOuterBorder  mainLoader">
         <img src={logo} alt="" />
       </div>}
